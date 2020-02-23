cc87f9eccff8bd892e6763f58ed84173d9c6f677
CROLFS001 <crolfs001@vplincsci049.ad.csbsju.edu>
2020-02-23 12:26:33 -0600

    Terminal 1 in Step 4: Modified Driver.java

--- Driver.java
+++ Driver.java
@@ -5,13 +5,14 @@ import static java.lang.System.*;
 
 public class Driver
 {  
+	
    public static void main(String[] args)
    {  
       // because of the import statement, we don't have to use
       // edu.csbsju.cs.Employee here
       Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
 
-      
+      harry.raiseSalary(10);
       // print out information about harry
       // use java.lang.System.out here
       out.println("name=" + harry.getName() + ",salary=" + harry.getSalary());
